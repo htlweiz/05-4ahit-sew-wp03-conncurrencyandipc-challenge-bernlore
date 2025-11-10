c1c54cd3deb4020b67b419d93ae5e6d6f17cecdc
bernlore <lorenzerbernd08@gmail.com>
2025-11-10 08:21:17 +0100

    2

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -1,17 +1,22 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
 namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-   
+        static int countA = 0;
+        static int countB = 100;
+       
     
     public static void Main(string[] args)
     {
+                
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        Thread threadA = new Thread(() => CountDownThreadB());
-        Thread threadB = new Thread(() => CountUpThreadA());
+        Thread threadA = new Thread(() => CountUpThreadA());
+        Thread threadB = new Thread(() => CountDownThreadB());
         threadA.Start();
         threadB.Start();
         threadA.Join();
@@ -20,22 +25,38 @@ class Program
         
         
     }
-    
+
     private static void CountUpThreadA()
     {
-        for (int i = 0; i <= 100; i++)
-      {
-         Thread.Sleep(100);
-      } 
-        
+
+        for (int i = 1; i <= 100; i++)
+        {
+            countA = i;
+            Thread.Sleep(100);
+            if (countA == countB)
+            {
+                Console.WriteLine("CountA und CountB sind gleich: " + countA);
+
+
+            }
+
+        }
     }
-    
-    private static void CountDownThreadB()
+     private static void CountDownThreadB()
     {
-         for (int i = 100; i >= 1; i--)
-      {
-         Thread.Sleep(100);
-      } 
-       
+
+        for (int i = 100; i >= 0; i--)
+        {
+            countB = i;
+            Thread.Sleep(100);
+            if (countA == countB)
+            {
+                Console.WriteLine("CountA und CountB sind gleich: " + countA);
+
+
+            }
+
+        }
     }
+ 
 }
